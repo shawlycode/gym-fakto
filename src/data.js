923c50a01e924a9c9c59d0906d231d534f741ce2
Shawlycode <kutinaorlando@gmail.com>
2023-04-21 14:24:06 +0000

    created dummy data

---
+++ src/data.js
@@ -0,0 +1,129 @@
+import { CgGym } from "react-icons/cg";
+import { GiGymBag } from "react-icons/gi";
+
+export const links = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "About",
+    path: "/about",
+  },
+  {
+    name: "Contact",
+    path: "/contact",
+  },
+  {
+    name: "Plans",
+    path: "/plans",
+  },
+  {
+    name: "Gallery",
+    path: "/gallery",
+  },
+  {
+    name: "Trainers",
+    path: "/trainers",
+  },
+  {
+    name: "NotFound",
+    path: "/notFound",
+  },
+];
+export const programs = [
+  {
+    id: 1,
+    icon: <CgGym />,
+    title: "Program 1",
+    info: "This is a package good for people who want to lose weight",
+    path: "/programs/111",
+  },
+  {
+    id: 1,
+    icon: <CgGym />,
+    title: "Program 2",
+    info: "This is a package good for people who want to lose weight",
+    path: "/programs/222",
+  },
+  {
+    id: 1,
+    icon: <CgGym />,
+    title: "Program 3",
+    info: "This is a package good for people who want to lose weight",
+    path: "/programs/333",
+  },
+  {
+    id: 1,
+    icon: <CgGym />,
+    title: "Program 4",
+    info: "This is a package good for people who want to lose weight",
+    path: "/programs/444",
+  },
+];
+
+export const faqs = [
+  {
+    id: 1,
+    question: "How do I benefit ?",
+    answer:
+      "Buy any pof our package from the plans available on the website and start right away",
+  },
+  {
+    id: 2,
+    question: "How do I benefit ?",
+    answer:
+      "Buy any pof our package from the plans available on the website and start right away",
+  },
+  {
+    id: 3,
+    question: "How do I benefit ?",
+    answer:
+      "Buy any pof our package from the plans available on the website and start right away",
+  },
+  {
+    id: 4,
+    question: "How do I benefit ?",
+    answer:
+      "Buy any pof our package from the plans available on the website and start right away",
+  },
+  {
+    id: 5,
+    question: "How do I benefit ?",
+    answer:
+      "Buy any pof our package from the plans available on the website and start right away",
+  },
+  {
+    id: 6,
+    question: "How do I benefit ?",
+    answer:
+      "Buy any pof our package from the plans available on the website and start right away",
+  },
+];
+
+export const values = [
+  {
+    id: 1,
+    icon: <GiGymBag />,
+    title: "Value 1",
+    desc: "This is a package good for people who want to lose weight",
+  },
+  {
+    id: 2,
+    icon: <GiGymBag />,
+    title: "Value 2",
+    desc: "This is a package good for people who want to lose weight",
+  },
+  {
+    id: 3,
+    icon: <GiGymBag />,
+    title: "Value 3",
+    desc: "This is a package good for people who want to lose weight",
+  },
+  {
+    id: 4,
+    icon: <GiGymBag />,
+    title: "Value 4",
+    desc: "This is a package good for people who want to lose weight",
+  },
+];
